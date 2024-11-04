d39a59f3f285e8d240e84f60b53585ed21d826f0
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2024-11-04 21:06:07 +0100

    add query transaction

---
+++ src/evm/transaction/query.ts
@@ -0,0 +1,25 @@
+import type {ExternalPure} from "src/evm/type-system/signature/method";
+import type {ExternalPureReturn} from "src/evm/type-system/signature/method";
+import type {ExternalView} from "src/evm/type-system/signature/method";
+import type {ExternalViewReturn} from "src/evm/type-system/signature/method";
+
+export type Query<T extends Array<unknown>> = {
+    type: "query";
+    to: string;
+    signature:
+        | ExternalPure
+        | ExternalPureReturn
+        | ExternalView
+        | ExternalViewReturn;
+    args?: T;
+};
+
+export const Query = <T extends Array<unknown>>({
+    type, 
+    to, 
+    signature, 
+    args}: Query<T>) => ({
+        type, 
+        to, 
+        signature, 
+        args});
